514ea87b5349146d750a3a90b174dd850e880764
odiselaker <odisseaspapanidis4@gmail.com>
2026-03-30 23:25:53 +0300

    apo macbook

---
+++ includes/footer.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="/Project-Kourio/CSS/style.css">
+    <link rel="stylesheet" href="/Project-Kourio/CSS/footer.css">
+    <title>Document</title>
+</head>
+<body>
+    <div class="footer color-gold p-40 ">
+        <footer>&copy; JustKoureio |Created by odiselaker</footer>
+    </div>
+    
+</body>
+</html>
