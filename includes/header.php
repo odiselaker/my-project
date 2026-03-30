33e8f89606bb2386ca7fc37143791941bd56e0e1
Odisseas Papanidis <odisseaspapanidis4@gmail.com>
2026-03-31 00:14:08 +0300

    apo windowsss

--- includes/header.php
+++ includes/header.php
@@ -5,14 +5,12 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Sofia&effect=neon|outline|emboss|shadow-multiple">
-    <link rel="stylesheet" href="/Project-Kourio/CSS/style.css">
-    <link rel="stylesheet" href="/Project-Kourio/CSS/header.css">
+    <link rel="stylesheet" href="/my-project/CSS/style.css">
+    <link rel="stylesheet" href="/my-project/CSS/header.css">
 </head>
 <body>
-    <div class="header bck-images-header bck-color-dark p-100 d-flex justify-center font-effect-emboss color-gold f-size-25 b-shadow ">
+    <div class="header bck-images-header bck-color-dark p-100 d-flex justify-center font-effect-emboss color-gold f-size-25 b-shadow  ">
         <h1>JustKoureio</h1>
-
-        
     </div>
 
 </body>
