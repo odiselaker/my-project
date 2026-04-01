f936c1e748c82f89f583054a9f9800dbab8446db
Odisseas Papanidis <odisseaspapanidis4@gmail.com>
2026-04-02 02:04:48 +0300

    allages apo windows

--- includes/header.php
+++ includes/header.php
@@ -4,14 +4,16 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Sofia&effect=neon|outline|emboss|shadow-multiple">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Monsieur+La+Doulaise&family=Parisienne&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="/my-project/CSS/style.css">
     <link rel="stylesheet" href="/my-project/CSS/header.css">
 </head>
 <body>
-    <div class="header bck-images-header bck-color-dark p-100 d-flex justify-center font-effect-emboss color-gold f-size-25 b-shadow  ">
-        <h1>JustKoureio</h1>
-    </div>
+  <div class="header d-flex justify-center items-center h-auto monsieur-la-doulaise-regular f-size-25 color-gold b-shadow">
+    <h1>JustKoureio</h1>
+</div>
 
 </body>
 </html>
